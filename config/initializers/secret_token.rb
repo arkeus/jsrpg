ef62ce5a92a454090c6f2f8ff6a887232429ca3b
Lee Miller <iarkeus@gmail.com>
2013-02-28 01:00:29 -0800

    Adding secret token. It's not a secret. YOU CAN HAVE IT.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,2 @@
+# This doesn't need to be secret. There are no cookies, etc etc. Rails is only used in this project to take advantage of easily hosting multiple projects via subdomain, assets pipeline with scss, and whatnot. HACK AWAY.
+JSRPG::Application.config.secret_token = '62fbc1e8ea329dfff95895042f04e40655e3cc675f727a3ca315022a68af9d4853dc92cfeb4a62a1142afe4f39373e2a43a3bb76dcef0c7f8461c6d76949a792'
