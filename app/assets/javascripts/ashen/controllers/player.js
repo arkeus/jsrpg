63cec8665fcd254fa92079d779ddc79696dd769f
Lee Miller <iarkeus@gmail.com>
2013-02-28 20:18:00 -0800

    Player controller annotation for injection

--- app/assets/javascripts/ashen/controllers/player.js
+++ app/assets/javascripts/ashen/controllers/player.js
@@ -1,3 +1,4 @@
-function PlayerController($scope, Registry) {
+var PlayerController = function($scope, Registry) {
     $scope.player = Registry.player;
-}
+};
+PlayerController.$inject = ["$scope", "Registry"];
