006540a3acee72d5f247d406cd0919c51518d4d5
Lee Miller <iarkeus@gmail.com>
2013-03-18 01:53:23 -0700

    Adding stones precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ JSRPG::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( ashen/manifest.css ashen/manifest.js )
+  config.assets.precompile += %w( ashen/manifest.css ashen/manifest.js stones/manifest.css stones/manifest.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
