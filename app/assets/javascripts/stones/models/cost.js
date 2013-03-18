3cb0195391bdbcbe8fe385d6dc910f2ed9b88375
Lee Miller <iarkeus@gmail.com>
2013-03-18 01:37:15 -0700

    Adding cost

---
+++ app/assets/javascripts/stones/models/cost.js
@@ -0,0 +1,7 @@
+var Cost = function(data) {
+    this.fire = data['fire'] || 0;
+    this.water = data['water'] || 0;
+    this.air = data['air'] || 0;
+    this.earth = data['earth'] || 0;
+    this.physical = data['physical'] || 0;
+};
--- app/assets/javascripts/stones/models/field.js
+++ app/assets/javascripts/stones/models/field.js
@@ -1,6 +1,6 @@
 var Field = function() {
     this.hero = WIZARD;
-    this.monsters = [SLIME, null, SLIME];
+    this.monsters = [null, null, null];
     this.hand = [SWORD, SWORD, FIREBALL, FIREBALL, FIREBALL, SLIME, SLIME];
     this.deck = [SWORD, SWORD, SWORD, SWORD, SWORD, SWORD, SWORD, SWORD, SWORD, SWORD, SWORD];
     this.graveyard = [];
