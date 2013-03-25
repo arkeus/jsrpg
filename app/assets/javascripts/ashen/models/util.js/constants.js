d388caa366c5a4afbfaf250c4a070801c2ebd0e4
Lee Miller <iarkeus@gmail.com>
2013-03-25 10:27:26 -0700

    Removing url for stones from readme, deleting utils

--- app/assets/javascripts/ashen/models/util.js/constants.js
+++
@@ -1,4 +0,0 @@
-/** 3 game ticks per second */
-var GAME_TICK = 1000;
-
-var SPELL_EXP_PER_CAST = 5;
--- app/assets/javascripts/ashen/models/util.js/utils.js
+++
@@ -1,5 +0,0 @@
-var Utils = new function() {
-    this.random = function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
-};
